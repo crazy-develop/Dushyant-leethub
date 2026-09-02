115b795b01e4128d7e53c9db59a36ea4f87460cb
Dushyant Saini <sainidushyant756@gmail.com>
2026-09-02 10:34:12 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.57%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int getValue(char s){
+        switch(s){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default : return 0;
+        }
+    }
+public:
+    int romanToInt(string s) {
+        int total = 0;
+    for(int i = 0 ; i <s.length() ;i++){
+if (getValue(s[i] )< getValue(s [i+1]) ){
+    total -=getValue(s[i]);
+}
+else
+total += getValue(s[i]);
+
+    }
+    return total;
+    }
+};
